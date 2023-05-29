3b3e8f5e303bc9484828946f7b90bb3946979a6c
kaiking06 <128317144+kaiking06@users.noreply.github.com>
2023-05-29 03:29:35 -0700

    new js line

---
+++ index.js
@@ -0,0 +1,28 @@
+const a = 20
+const b = 100
+const result = a+b
+console.log(result)
+
+const c = 20
+const d = 100
+const result1 = c-d
+console.log(result1)
+
+const e = 20
+const f = 100
+const result2 = e*f
+console.log(result2)
+
+const g = 20
+const h = 100
+const result3 = g/h
+console.log(result3)
+
+
+const sentence ="hello world"
+console.log(sentence)
+
+const stament  = "my name is luther"
+console.log(stament)
+
+console.log(sentence + stament);
